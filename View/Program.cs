18be7f0b2dc38ca1686bf36a0280aa97994c5d13
Luiz Gustavo Banin De Sena <luizgbs1@gmail.com>
2021-03-17 16:17:06 -0300

    Upando

---
+++ View/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ChainofResponsibility;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    class Program
+    {
+        static void Main()
+        {
+            new ClientChainofRespons().Main();
+            Console.ReadKey();
+        }
+    }
+}
